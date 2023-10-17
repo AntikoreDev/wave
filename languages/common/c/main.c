d3873786720af66d47bfcbdf73e70bda7c505518
Antikore <koopatropa8@hotmail.com>
2023-10-17 21:54:58 +0200

    Remove main method
    To keep the repo consistent

--- languages/common/c/main.c
+++ languages/common/c/main.c
@@ -13,17 +13,3 @@ double wave_no_offset(double from, double to, double duration) {
     double current_time = (double)clock() / CLOCKS_PER_SEC;
     return from + a4 + sin((current_time / duration) * (M_PI * 2)) * a4;
 }
-
-int main() {
-    double from = -10.0;
-    double to = 10.0;
-    double duration = 12.0;
-    double offset = 0.5;
-
-    while (1) {
-        double result = wave(from, to, duration, offset);
-        printf("Result: %lf\n", result);
-    }
-
-    return 0;
-}
